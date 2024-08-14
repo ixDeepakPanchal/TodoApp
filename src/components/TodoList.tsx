7e82e6d5a9cd1724e1be5790bed68e8556a2209d
gitHub-work <deepak.panchal@innovatechs.com>
2024-08-14 14:42:35 +0530

    adding gap

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -30,7 +30,7 @@ const TodoList = ({
     <LoadingPage />
   ) : (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 2xl:grid-cols-4 gap-6 px-10 py-8 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 2xl:grid-cols-4 gap-10 px-10 py-8 ">
         {todos.map(
           (item: {
             completed: boolean;
